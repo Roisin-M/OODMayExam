2ff173c04c673e7a841cd67cb4ece71832d01a65
Roisin-M <S00223865@atu.ie>
2024-05-13 13:31:13 +0100

    createed classes and modelled one to any relationship, added to database

---
+++ DatabaseManagement/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OODMayExam;
+
+namespace DatabaseManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create Db Model
+            RestaurantData db = new RestaurantData("OODExam_RoisinMuldoon");
+            //create some Bookings
+            Booking b1 = new Booking() { BookingsDate = new DateTime(2024, 05, 13), NumberOfParticipants = 2 };
+            Booking b2 = new Booking() { BookingsDate = new DateTime(2024, 05, 10), NumberOfParticipants = 5 };
+            Booking b3 = new Booking() { BookingsDate = new DateTime(2024, 05, 11), NumberOfParticipants = 8 };
+
+            //create some Customers
+            Customer c1 = new Customer() { Name = "Tom Jones", ContactNumber = "086-123 4567" };
+            Customer c2 = new Customer() { Name = "Mary Smith", ContactNumber = "086 546 3214" };
+            Customer c3 = new Customer() { Name = "Jo Doyle", ContactNumber = "087 1221 222" };
+            //add bookings to customers
+            c1.Bookings.Add(b1);
+            c2.Bookings.Add(b2);
+            c3.Bookings.Add(b3);
+
+            //add to db
+            db.Bookings.Add(b1);
+            db.Bookings.Add(b2);
+            db.Bookings.Add(b3);
+            db.Customers.Add(c1);
+            db.Customers.Add(c2);
+            db.Customers.Add(c3);
+
+            //save changes
+            db.SaveChanges();
+        }
+    }
+}
---
+++ OODMayExam/Booking.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OODMayExam
+{
+    public class Booking
+    {
+        //properties
+        public int BookingId { get; set; }
+        public DateTime BookingsDate { get; set; }
+        public int NumberOfParticipants { get; set; }
+        //foreign key id
+        public int CustomerId { get; set; }
+        //navigation property
+        public virtual Customer Customer { get; set; }
+
+    }
+    public class Customer
+    {
+        //properties
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string ContactNumber { get; set; }
+        //A customer can have many Bookings
+        //Model a one-to-many relationship
+        public List<Booking> Bookings { get; set; }
+
+        //constructors
+        public Customer()
+        {
+            Bookings = new List<Booking>();
+        }
+    }
+    public class RestaurantData : DbContext
+    {
+        //comstructor
+        public RestaurantData(string databaseName):base(databaseName) { }
+        // 2 dbSets properties
+        public DbSet<Booking> Bookings { get; set;}
+        public DbSet<Customer> Customers { get;set; }
+    }
+}
