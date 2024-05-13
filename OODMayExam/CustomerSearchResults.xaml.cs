4b959be68097c33d44d9fe58575dd07e123a5e6b
Roisin-m <S00223865@atu.ie>
2024-05-13 15:15:46 +0100

    Final commit

---
+++ OODMayExam/CustomerSearchResults.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OODMayExam
+{
+    /// <summary>
+    /// Interaction logic for CustomerSearchResults.xaml
+    /// </summary>
+    public partial class CustomerSearchResults : Window
+    {
+        private RestaurantData db = new RestaurantData("OODExam_RoisinMuldoon");
+        private List<Customer> _customers;
+
+        public CustomerSearchResults(List<Customer> customers)
+        {
+            InitializeComponent();
+            _customers = customers;
+            PopulateCustomerListBox();
+        }
+
+        private void PopulateCustomerListBox()
+        {
+            lbx_Customers.Items.Clear(); // Clear existing items if any
+
+            foreach (var customer in _customers)
+            {
+                string displayText = $"{customer.Name} ({customer.ContactNumber})";
+                lbx_Customers.Items.Add(displayText);
+            }
+        }
+    }
+}
--- OODMayExam/MainWindow.xaml.cs
+++ OODMayExam/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,60 @@ namespace OODMayExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private RestaurantData db = new RestaurantData("OODExam_RoisinMuldoon");
         public MainWindow()
         {
 
             InitializeComponent();
         }
+        private void dp_Date_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dp_Date.SelectedDate.HasValue)
+            {
+                DateTime selectedDate = dp_Date.SelectedDate.Value;
+                UpdateBookingList(selectedDate);
+                UpdateAvailableAndCapacity(selectedDate);
+            }
+        }
+
+        private void UpdateBookingList(DateTime date)
+        {
+            var bookings = db.Bookings
+                             .Include(b => b.Customer)  
+                             .Where(b => DbFunctions.TruncateTime(b.BookingsDate) == date.Date)
+                             .ToList();
+
+            lbx_BookingDetails.Items.Clear();
+
+            foreach (var booking in bookings)
+            {
+                string displayText = $"{booking.Customer.Name} ({booking.Customer.ContactNumber}) - Party of {booking.NumberOfParticipants}";
+                lbx_BookingDetails.Items.Add(displayText);
+            }
+        }
+
+        private void UpdateAvailableAndCapacity(DateTime date)
+        {
+             const int TOTAL_CAPACITY = 40;  
+            int bookedSeats = db.Bookings
+                                 .Where(b => DbFunctions.TruncateTime(b.BookingsDate) == date.Date)
+                                 .Sum(b => b.NumberOfParticipants);
+
+            int availableSeats = TOTAL_CAPACITY - bookedSeats;
+
+            tblk_Capacity.Text = TOTAL_CAPACITY.ToString();
+            tblk_NumBookings.Text = bookedSeats.ToString();
+            tblk_Available.Text = availableSeats.ToString();
+        }
+        private void btn_CustomerSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string customerName = tb_CustomerName.Text;
+            var customers = db.Customers.Where(c => c.Name.Contains(customerName)).ToList();
+
+            // Assuming CustomerSearchResults is initialized to take a list of customers
+            var searchWindow = new CustomerSearchResults(customers);
+            searchWindow.Show();
+        }
+
     }
 }
